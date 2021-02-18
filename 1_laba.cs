1db51934388b08f4e0ea1225fe711b7532601899
Yekaterina Silichenko <e.silichenko@yandex.by>
2021-02-18 14:21:51 +0300

    Use proper grammar.

--- 1_laba.cs
+++ 1_laba.cs
@@ -7,10 +7,10 @@ namespace _1lab
         static void Main(string[] args)
         {
             string name;
-            Console.WriteLine("Enter your first name: ");
-            name = Console.ReadLine();=
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
             Console.WriteLine("Hello {0}", name);
-            Console.WriteLine("Hello {0}", name);
+            Console.WriteLine("Hello, {0}", name);
             Console.ReadLine();
         }
     }
