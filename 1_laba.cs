beaa7c51f7046462ae23c1ec6efbdceb8a7543e1
Yekaterina Silichenko <e.silichenko@yandex.by>
2021-02-18 14:00:07 +0300

    Initial commit.

---
+++ 1_laba.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.ReadLine();
+        }
+    }
+}
+
