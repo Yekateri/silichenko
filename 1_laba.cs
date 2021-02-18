a75989df9a13f7c0f8a236667771dc71a982d53a
Yekaterina Silichenko <e.silichenko@yandex.by>
2021-02-18 14:18:15 +0300

    Signed-off-by: Yekaterina Silichenko <e.silichenko@yandex.by>

--- 1_laba.cs
+++ 1_laba.cs
@@ -7,9 +7,10 @@ namespace _1lab
         static void Main(string[] args)
         {
             string name;
-            Console.WriteLine("Enter your name: ");
-            name = Console.ReadLine();
-            Console.WriteLine("Hello {0}, name");
+            Console.WriteLine("Enter your first name: ");
+            name = Console.ReadLine();=
+            Console.WriteLine("Hello {0}", name);
+            Console.WriteLine("Hello {0}", name);
             Console.ReadLine();
         }
     }
